c92e042a88bf72f2c2c76d919d843ecabfc76ad6
rhard32 <rhard32@gmail.com>
2015-04-23 16:53:22 -0400

    Adding code to Logger

---
+++ ContosoUniversity/Logging/Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+
+namespace ContosoUniversity.Logging
+{
+    public class Logger : ILogger
+    {
+        public void Information(string message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        public void Information(string fmt, params object[] vars)
+        {
+            Trace.TraceInformation(fmt, vars);
+        }
+        public void Inforation(Exception exception, string fmt, params object[] vars )
+        {
+            Trace.TraceInformation(fmt, vars);
+        }
+        public void Information(Exception exception, string fmt, params object[] vars)
+        {
+            Trace.TraceInformation(FormatExceptionMessage(exception, fmt, vars));
+        }
+        public void Warning(string fmt, params object[] vars)
+        {
+            Trace.TraceWarning(FormatExceptionMessage(exception, fmt, vars));
+        }
+        public void Error(string message)
+        {
+            Trace.TraceError(message);
+
+        }
+        public void Error(string fmt, params object[] vars)
+        {
+            Trace.TraceError(fmt, vars);
+        }
+
+        public void Error(Exception exception, string fmt, params object[] vars)
+        {
+            Trace.TraceError(FormatExceptionMessage(exception, fmt, vars));
+        }
+
+    }
+}
