2fc93966404afe3429f70e9b88280ae37f42ecbc
rhard32 <rhard32@gmail.com>
2015-04-23 10:52:23 -0400

    Adding Dispose method to the home controller

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -35,5 +35,11 @@ namespace ContosoUniversity.Controllers
 
             return View();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
