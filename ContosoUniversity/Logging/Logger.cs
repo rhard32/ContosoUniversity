b15071a997c2e41847775c4e157a9de50dc57b01
rhard32 <rhard32@gmail.com>
2015-04-24 09:53:14 -0400

    Finishing the Logger code

--- ContosoUniversity/Logging/Logger.cs
+++ ContosoUniversity/Logging/Logger.cs
@@ -24,7 +24,7 @@ namespace ContosoUniversity.Logging
         {
             Trace.TraceInformation(FormatExceptionMessage(exception, fmt, vars));
         }
-        public void Warning(string fmt, params object[] vars)
+        public void Warning(Exception exception, string fmt, params object[] vars)
         {
             Trace.TraceWarning(FormatExceptionMessage(exception, fmt, vars));
         }
@@ -42,6 +42,29 @@ namespace ContosoUniversity.Logging
         {
             Trace.TraceError(FormatExceptionMessage(exception, fmt, vars));
         }
+        public void TraceApi(string componentName, string method, TimeSpan timespan)
+        {
+            TraceApi(componentName, method, timespan, "");
+        }
+        public void TraceApi(string componentName, string method, TimeSpan timespan, string fmt, params object[] vars)
+        {
+            TraceApi(componentName, method, timespan, string.Format(fmt, vars));
+        }
+        public void TraceApi(string componentName, string method, TimeSpan timespan, string properties)
+        {
+            string message = String.Concat("Component:", componentName, ";Method:", method, ";Timespan:", timespan.ToString(), ";Properties;", properties);
+            Trace.TraceInformation(message);
+        }
+        private static string FormatExceptionMessage(Exception exception, string fmt, object[] vars)
+        {
+            //Simple exception formatting: for a more comprehensive version see
+            //http://code.msdn.microsoft.com/windowsazure/Fix-it-app-for-Building-cdd80df4
+            var sb = new StringBuilder();
+            sb.Append(string.Format(fmt, vars));
+            sb.Append(" Exception: ");
+            sb.Append(exception.ToString());
+            return sb.ToString();
 
+        }
     }
 }
