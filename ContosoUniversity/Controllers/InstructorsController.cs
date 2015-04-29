fe6620356bb0b32cbc25eb0ef92bc965917ffa61
rhard32 <rhard32@gmail.com>
2015-04-29 16:31:32 -0400

    Changed Index controller due to failed build

--- ContosoUniversity/Controllers/InstructorsController.cs
+++ ContosoUniversity/Controllers/InstructorsController.cs
@@ -17,27 +17,24 @@ namespace ContosoUniversity.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Instructors
-        public ActionResult Index(int? id, int? courseID)
-        {
-            var viewModel = new InstructorIndexData();
-            viewModel.Instructors = db.Instructors
-                .Include(i => i.OfficeAssignment)
-                .Include(i => i.Courses.Select(c => c.Department))
-                .OrderBy(i => i.LastName);
-           
-        }
-
-         if ( id != null)
-    {
-        ViewBag.InstructorID = id.Value;
- viewModel.Courses = viewModel.Instructors.Where(
- i => i.ID == id.Value).Single().Courses;
-    }
-    if (courseID != null)
+       public ActionResult Index(int? id, int? courseID)
+{
+ var viewModel = new InstructorIndexData();
+ viewModel.Instructors = db.Instructors
+ .Include(i => i.OfficeAssignment)
+ .Include(i => i.Courses.Select(c => c.Department))
+ .OrderBy(i => i.LastName);
+ if (id != null)
  {
- ViewBag.CourseID = courseID.Value;
- viewModel.Enrollments = viewModel.Courses.Where(
- x => x.CourseID == courseID).Single().Enrollments;
+     ViewBag.InstructorID = id.Value;
+     viewModel.Courses = viewModel.Instructors.Where(
+     i => i.ID == id.Value).Single().Courses;
+ }
+ if (courseID != null)
+ {
+     ViewBag.CourseID = courseID.Value;
+     viewModel.Enrollments = viewModel.Courses.Where(
+     x => x.CourseID == courseID).Single().Enrollments;
  }
  return View(viewModel);
 }
