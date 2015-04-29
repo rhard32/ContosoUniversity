56812c890b3c8e4e819c5e90b865d22b445d8d3c
rhard32 <rhard32@gmail.com>
2015-04-29 15:18:42 -0400

    Setting properties for  InstructorIndexData.cs

--- ContosoUniversity/ViewModels/InstructorIndexData.cs
+++ ContosoUniversity/ViewModels/InstructorIndexData.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using ContosoUniversity.Models;
 
 namespace ContosoUniversity.ViewModels
 {
     public class InstructorIndexData
     {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
     }
 }
